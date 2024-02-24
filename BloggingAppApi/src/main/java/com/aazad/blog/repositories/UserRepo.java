dc0c93060396901ee4034ba6104355b7e5d5f165
BableshAAzad <bableshaazad@outlook.com>
2024-02-24 14:02:22 +0530

    Api Creation and check done

---
+++ BloggingAppApi/src/main/java/com/aazad/blog/repositories/UserRepo.java
@@ -0,0 +1,9 @@
+package com.aazad.blog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aazad.blog.entities.User;
+
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+}
---
+++ BloggingAppApi/src/test/java/com/aazad/blog/BloggingAppApiApplicationTests.java
@@ -0,0 +1,27 @@
+package com.aazad.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.aazad.blog.repositories.UserRepo;
+
+@SpringBootTest
+class BloggingAppApiApplicationTests {
+
+	@Autowired
+	private UserRepo userRepo;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void repoTest() {
+		String className = this.userRepo.getClass().getName();
+		String packegeName = this.userRepo.getClass().getPackageName();
+		System.out.println("Class Name : " + className);
+		System.out.println("Package Name : " + packegeName);
+	}
+
+}
